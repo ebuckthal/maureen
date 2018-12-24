800d53d5ef8f216d928b22a59ec2edfcbc4fe986
Eric Buckthal <eric@simplecontacts.com>
2018-12-24 09:06:33 -0700

    Updates

---
+++ precache-manifest.add43937b5510ead9c32a469b7c81364.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2db24a1cc10076b6bee",
+    "url": "/maureen/static/js/runtime~main.a2db24a1.js"
+  },
+  {
+    "revision": "dc07f5632669869ca6cd",
+    "url": "/maureen/static/js/main.dc07f563.chunk.js"
+  },
+  {
+    "revision": "3f05fb1a76efc4c61d07",
+    "url": "/maureen/static/js/1.3f05fb1a.chunk.js"
+  },
+  {
+    "revision": "dc07f5632669869ca6cd",
+    "url": "/maureen/static/css/main.984ad479.chunk.css"
+  },
+  {
+    "revision": "fed6cfc326aab6ec275e531c8b64485b",
+    "url": "/maureen/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/maureen/precache-manifest.add43937b5510ead9c32a469b7c81364.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/maureen/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
