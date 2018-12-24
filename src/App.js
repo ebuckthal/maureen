0a60d60820c2b522dd578786b0c15997c580f230
Eric Buckthal <eric@simplecontacts.com>
2018-12-24 09:03:25 -0700

    deploy attempt

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useContext, useState } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+
+import portfolio from './portfolio.json';
+
+const SliderContext = createContext([0, () => {}]);
+
+const SliderContextProvider = props => {
+  const [sliderValue, setSliderValue] = useState(3);
+  return (
+    <SliderContext.Provider value={[sliderValue, setSliderValue]}>
+      {props.children}
+    </SliderContext.Provider>
+  );
+};
+
+const style = css`
+  color: hotpink;
+`;
+
+const App = () => {
+  return (
+    <SliderContextProvider>
+      <Slider />
+      <VisibleArea />
+    </SliderContextProvider>
+  );
+};
+
+const Slider = () => {
+  const [sliderValue, setSliderValue] = useContext(SliderContext);
+  return (
+    <div>
+      <input
+        type="range"
+        min="0"
+        max="6"
+        value={sliderValue}
+        onChange={e => setSliderValue(parseInt(e.currentTarget.value, 10))}
+      />
+    </div>
+  );
+};
+
+const Thumbnail = props => {
+  const [sliderValue] = useContext(SliderContext);
+
+  const [min, max] = props.range;
+  if (sliderValue < min || sliderValue > max) {
+    return null;
   }
-}
+
+  return <div>{props.children}</div>;
+};
+
+const THUMBNAILS = [
+  <Thumbnail range={[0, 2]}>illustration</Thumbnail>,
+  <Thumbnail range={[2, 5]}>lil of both</Thumbnail>,
+  <Thumbnail range={[3, 3]}>home</Thumbnail>,
+  <Thumbnail range={[5, 6]}>science</Thumbnail>,
+];
+
+const VisibleArea = () => {
+  return <div>{THUMBNAILS}</div>;
+};
 
 export default App;
